dc9fd7e665b747fa44e823760e84c500728a619e
daedayo <ademefundaedayo@yahoo.com>
2022-10-25 21:21:51 -0400

    second fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        String name = "Ade";
+        String name = "vamsi";
         System.out.println(name);
     }
-}
-
 }
